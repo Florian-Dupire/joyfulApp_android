439ec87fdd4047c9dfa21fcd6778236c1f65fca7
Florian-Dupire <125898280+Florian-Dupire@users.noreply.github.com>
2024-03-25 18:02:35 +0100

    feat: (with new engine) navigate to Flutter screens

--- app/src/main/java/com/example/joyfulapp/MainActivity.kt
+++ app/src/main/java/com/example/joyfulapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.joyfulapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1: Button = findViewById(R.id.buttonBlueScreen)
+        val button2: Button = findViewById(R.id.buttonPinkScreen)
+
+        // Set click listeners for each button
+        button1.setOnClickListener {
+            navigateToFlutterScreen("/blueScreen")
+        }
+
+        button2.setOnClickListener {
+            navigateToFlutterScreen("/pinkScreen")
+        }
+    }
+
+    private fun navigateToFlutterScreen(route: String) {
+        startActivity(
+            FlutterActivity
+                .withNewEngine()
+                .initialRoute(route)
+                .build(this)
+        );
     }
 }
