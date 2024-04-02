14f5006c84f84ed444336c5b306cdbbc50f0bc7f
Florian-Dupire <125898280+Florian-Dupire@users.noreply.github.com>
2024-04-02 12:37:15 +0200

    feat: use of flutter fragment

--- app/src/main/java/com/example/joyfulapp/NativeGreenScreenActivity.kt
+++ app/src/main/java/com/example/joyfulapp/NativeGreenScreenActivity.kt
@@ -3,6 +3,8 @@ package com.example.joyfulapp
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
+import io.flutter.embedding.android.FlutterFragment
 
 class NativeGreenScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +22,18 @@ class NativeGreenScreenActivity : AppCompatActivity() {
         findViewById<Button>(R.id.buttonPinkScreen).setOnClickListener {
             JoyRouter.navigateTo("/pinkScreen", this)
         }
+
+        initFlutterFragment()
+    }
+
+    private fun initFlutterFragment() {
+
+        val flutterFragment: FlutterFragment = FlutterFragment.withNewEngineInGroup(JoyRouter.engineGroupId)
+            .initialRoute("/orangeSquare")
+            .build()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, flutterFragment)
+            .commit()
     }
 }
